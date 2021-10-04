78fce0097a34f39e56e22a05417e290e55dd59cf
Gunkel <gunkeec@dunwoody.edu>
2021-10-03 19:47:27 -0500

    Renamed files and imports

--- mainprogram.py
+++ mainprogram.py
@@ -1,4 +1,4 @@
-import problem1, problem2, problem3, problem4, problem5, problem6,problem7, problem8
+import Cylinder, Cube, Cuboid, Triangle, Trapezoid, Sphere,EquilateralTriangle, Cone
 
 def main():
 
@@ -7,21 +7,21 @@ def main():
         print("1. Sphere \n2. Cylinder\n3. Cone\n4. Cube\n5. Triangle\n6. Trapezoid\n7.Cuboid\n8. Equilateral triangle\n0. Quit")
         selection = int(input("Please enter your selection: "))
         if selection == 1:
-            problem1.main()
+            Sphere.main()
         if selection == 2:
-            problem2.main()
+            Cylinder.main()
         if selection == 3:
-            problem3.main()
+            Cone.main()
         if selection == 4:
-            problem4.main()
+            Cube.main()
         if selection == 5:
-            problem5.main()
+            Triangle.main()
         if selection == 6:
-            problem6.main()
+            Trapezoid.main()
         if selection == 7:
-            problem7.main()
+            Cuboid.main()
         if selection == 8:
-            problem8.main()
+            EquilateralTriangle.main()
         if selection == 0:
             break
 
