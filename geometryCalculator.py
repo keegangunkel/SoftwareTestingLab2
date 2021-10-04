ce9d37a686733aa6323e02d4f1fb153e0d0fa1e1
Gunkel <gunkeec@dunwoody.edu>
2021-10-04 00:13:56 -0500

    Cleaned up some wording

--- geometryCalculator.py
+++ geometryCalculator.py
@@ -2,7 +2,7 @@ import cylinder, cube, cuboid, triangle, trapezoid, sphere,equilateralTriangle, 
 
 def pick(selection):
     while True:
-        print("\nWelcome to my Geometry Program")
+        print("\nWelcome to my Geometry Calculator")
         print("1. Sphere \n2. Cylinder\n3. Cone\n4. Cube\n5. Triangle\n6. Trapezoid\n7. Cuboid\n8. Equilateral triangle\n0. Quit")
         selection = int(input("Please enter your selection: "))
         if selection == 1:
